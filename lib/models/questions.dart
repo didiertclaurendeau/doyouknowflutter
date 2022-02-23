f1667566a5e6092bad3f2de5c651a575ee1dbe50
Didier Tremblay <didier@tremblay-trepanier.com>
2022-02-23 08:27:49 -0500

    Ajout de question pour avoir un plus de matériel

--- lib/models/questions.dart
+++ lib/models/questions.dart
@@ -1,18 +1,19 @@
-/* Quelques idees 
+/* quelques idées 
 
   - Avoir les questions dans un serveur
   - Afficher un "RichText" pour mettre le code avec "syntax highlight"
-  - Supporter plusieur langues
-  - Sauvegarder les resultats
-  - Choix Multiple
+  - Supporter plusieurs langues
+  - Sauvegarder les résultats
+  - Choix multiple
   - 
 */
 
 /*
-  A faire, afficher les questions aleatoirement.
+  A faire, afficher les questions aléatoirement.
 */
 
 /*
+{
     "source": "",
     "Question": """
 """,
@@ -21,8 +22,9 @@
     ],
     "reponse": ,
     "point": 
-*/
+    },
 
+*/
 
 const listeDeQuestions = [
   {
@@ -49,13 +51,97 @@ printName(String name, {String? lastName}){
     "Question": """
 quels sont les composants coulissants?
 """,
+    "Choix": ["ListView", "GridView", "Container", "Center"],
+    "reponse": 1,
+    "point": 1
+  },
+  {
+    "source": "Didier",
+    "Question": """
+Un Future en Dart c'est:
+""",
     "Choix": [
-      "ListView",
-      "GridView",
-      "Container",
-      "Center"
+      "Un objet qui attend une opération synchrone et retourne un résultat",
+      "Un objet qui attend une opération asynchrone et retourne un résultat",
+      "Un widget qui a une forme bleue rectangulaire",
+      "Un objet qui permet de recevoir une transaction http"
+    ],
+    "reponse": 2,
+    "point": 1
+  },
+  {
+    "source": "Didier",
+    "Question": """
+Qu'est ce qui est exact concernant la méthode build de la class Widget
+""",
+    "Choix": [
+      "Build sera réexecuté fréquemment pour construire l'arbre de widget",
+      "Build est réexecuté constamment pour générer l'interface graphique",
+      "Build permet à Flutter d'évaluer s’il doit générer l'interface"
     ],
     "reponse": 1,
     "point": 1
-  }
+  },
+  {
+    "source": "Didier",
+    "Question": """
+Dans un widget l'attribut keys sert à
+""",
+    "Choix": [
+      "Activer les permissions pour afficher un Widget",
+      "Permets de maintenir les listes de widget contenu dans votre widget",
+      "Permet a Flutter d'identifié le widget de façon unique dans l'élément tree"
+    ],
+    "reponse": 3,
+    "point": 1
+  },
+  {
+    "source": "Didier",
+    "Question": """
+La classe MediaQuery permet de....
+""",
+    "Choix": [
+      "Mettre en place des restrictions pour les appareils mobiles",
+      "Permets de récupérer des informations sur l'appareil mobile",
+      "Permets de gérer les images et vidéos dans l'application"
+    ],
+    "reponse": 2,
+    "point": 1
+  },
+  {
+    "source": "Didier",
+    "Question": """
+Je permets de récupérer la taille disponible pour l'affichage d'une Widget
+""",
+    "Choix": ["MediaQuery", "Container", "LayoutBuilder"],
+    "reponse": 3,
+    "point": 1
+  },
+  {
+    "source": "Didier",
+    "Question": """
+Cuppertino est
+""",
+    "Choix": [
+      "Une marque de café",
+      "Des widgets pour afficher des cubes",
+      "Une Librairie respectant le style d'application Android",
+      "Une librairie respectant le style d'application IOS"
+    ],
+    "reponse": 4,
+    "point": 1
+  },
+  {
+    "source": "Didier",
+    "Question": """
+Qu'est-ce qui est vrai avec les Widget Row et Column
+""",
+    "Choix": [
+      "Permet de mettre des éléments qui défile en Rangé et Colonne",
+      "Reçoivent une liste de Widget enfants",
+      "Sont des widget qui peuvent recevoir des styles variés"
+    ],
+    "reponse": 2,
+    "point": 1
+  },
 ];
